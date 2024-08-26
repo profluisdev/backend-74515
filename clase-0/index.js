8922c87bb7dc5b4a8ea49f7e92dc6d7089f01e76
Luis Mera <profeluismeradev@gmail.com>
2024-08-26 19:29:44 -0300

    clase 0

---
+++ clase-0/index.js
@@ -0,0 +1,25 @@
+
+// String
+let nombre = "Luis";
+console.log(`String: ${nombre}`);
+
+// Booleans
+let aprobado = true;
+console.log(`Boolean: ${aprobado}`);
+
+// Number
+let edad = 30;
+console.log(`Number: ${edad}`);
+console.log("Number: " + edad);
+
+// Object
+let persona = {
+    nombre: "Juan",
+    apellido: "Perez"
+};
+console.log(`Object: ${persona}`);
+console.log(persona);
+
+// Array
+let numeros = [1,2,3,4,5];
+console.log(numeros);
